8ed76028e6c47fce2f91cdd1e83a5dd711b4d285
Yianni Galiatsatos <Ygaliatsatos@gmail.com>
2025-07-02 21:53:56 +0000

    reduce the max height of the page

--- src/app/contrast-checker/page.tsx
+++ src/app/contrast-checker/page.tsx
@@ -4,7 +4,7 @@ import { ContrastChecker } from '@/components/colors/ContrastChecker';
 
 export default function ContrastCheckerPage() {
   return (
-    <main className="flex-1 w-full max-w-7xl mx-auto p-4 md:p-8">
+    <main className="w-full max-w-7xl mx-auto p-4 md:p-8">
       <ContrastChecker />
     </main>
   );
