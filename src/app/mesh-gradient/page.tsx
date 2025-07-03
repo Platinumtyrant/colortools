0dab1ec1eab5c554cf1792d82bdf3fd69305e9bf
Yianni Galiatsatos <Ygaliatsatos@gmail.com>
2025-07-03 01:44:14 +0000

    allow for two containers to be side-side at their full width

--- src/app/mesh-gradient/page.tsx
+++ src/app/mesh-gradient/page.tsx
@@ -3,7 +3,7 @@ import { GradientMeshBuilder } from '@/components/colors/GradientMeshBuilder';
 
 export default function MeshGradientPage() {
   return (
-    <main className="flex-1 w-full max-w-7xl mx-auto p-4 md:p-8">
+    <main className="flex-1 w-full p-4 md:p-8">
         <GradientMeshBuilder />
     </main>
   );
