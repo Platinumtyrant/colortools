b483a545b6989c47ee58d19c44ce99cc76f69f59
Yianni Galiatsatos <Ygaliatsatos@gmail.com>
2025-07-04 23:09:40 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -1,23 +1,15 @@
 "use client"
 
-import * as React from "react"
 import { GripVertical } from "lucide-react"
-import {
-  ResizablePanelGroup as ResizablePanelGroupPrimitive,
-  ResizablePanel as ResizablePanelPrimitive,
-  ResizableHandle as ResizableHandlePrimitive,
-  type ResizablePanelProps,
-  type ResizableHandleProps,
-  type ResizablePanelGroupProps,
-} from "react-resizable-panels"
+import * as ResizablePrimitive from "react-resizable-panels"
 
 import { cn } from "@/lib/utils"
 
 const ResizablePanelGroup = ({
   className,
   ...props
-}: ResizablePanelGroupProps) => (
-  <ResizablePanelGroupPrimitive
+}: React.ComponentProps<typeof ResizablePrimitive.PanelGroup>) => (
+  <ResizablePrimitive.PanelGroup
     className={cn(
       "flex h-full w-full data-[panel-group-direction=vertical]:flex-col",
       className
@@ -26,14 +18,16 @@ const ResizablePanelGroup = ({
   />
 )
 
-const ResizablePanel = ResizablePanelPrimitive
+const ResizablePanel = ResizablePrimitive.Panel
 
 const ResizableHandle = ({
   withHandle,
   className,
   ...props
-}: ResizableHandleProps & { withHandle?: boolean }) => (
-  <ResizableHandlePrimitive
+}: React.ComponentProps<typeof ResizablePrimitive.PanelHandle> & {
+  withHandle?: boolean
+}) => (
+  <ResizablePrimitive.PanelHandle
     className={cn(
       "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=vertical]>div]:rotate-90",
       className
@@ -45,7 +39,7 @@ const ResizableHandle = ({
         <GripVertical className="h-2.5 w-2.5" />
       </div>
     )}
-  </ResizableHandlePrimitive>
+  </ResizablePrimitive.PanelHandle>
 )
 
 export { ResizablePanelGroup, ResizablePanel, ResizableHandle }
