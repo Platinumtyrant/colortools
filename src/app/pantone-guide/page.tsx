e9f848559d614ab9e9c6d1fbf086690d53152dd8
Yianni Galiatsatos <Ygaliatsatos@gmail.com>
2025-07-05 20:37:02 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/pantone-guide/page.tsx
+++ src/app/pantone-guide/page.tsx
@@ -1,2 +1,5 @@
-// This file is obsolete and will be deleted.
-// The Pantone Guide has been refactored into separate pages.
+// This page is now a dropdown menu in the header.
+// This empty component prevents an error if this URL is navigated to directly.
+export default function PantoneGuidePage() {
+  return null;
+}
